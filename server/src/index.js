6257524815c853c19915700d7d0339aaa5118180
gersonjuniordev <gersonjunior125@gmail.com>
2026-03-19 10:48:14 -0300

    att

--- server/src/index.js
+++ server/src/index.js
@@ -36,7 +36,7 @@ app.use(
     },
     credentials: false,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
